95fba30dc9ba32d40147951e893bef6cd54a5a37
Rudy Banda <rudy.banda1@pcc.edu>
2017-10-23 21:10:48 -0700

    added main methord that prints hello, world

--- src/edu/pcc/cishelloworld/HelloWorld.java
+++ src/edu/pcc/cishelloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cishelloworld;
 
+/**
+ * Created by Rudy Banda on 10/23/2017
+ */
 public class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("hello, world");
+    }
 }
+
